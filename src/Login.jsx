db6a58e6682e108bdbd796a7490baf65c387aa43
JorgeSalgado0999 <jorgesalgado_isc@outlook.com>
2022-11-23 13:49:19 -0600

    add formhandler

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,24 +5,24 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 export const Login = () => {
+	function formHandler(e) {
+		e.preventDefault();
+		//aqui va el código de login
+		console.log("formHandler");
+	}
+
 	return (
 		<div className="App">
 			<h1>Inicia Sesión</h1>
-			<Form>
+			<Form onSubmit={(e) => formHandler(e)}>
 				<Form.Group className="mb-3" controlId="formBasicEmail">
 					<Form.Label>Email address</Form.Label>
 					<Form.Control type="email" placeholder="Enter email" />
-					<Form.Text className="text-muted">
-						We'll never share your email with anyone else.
-					</Form.Text>
 				</Form.Group>
 
 				<Form.Group className="mb-3" controlId="formBasicPassword">
 					<Form.Label>Password</Form.Label>
 					<Form.Control type="password" placeholder="Password" />
-				</Form.Group>
-				<Form.Group className="mb-3" controlId="formBasicCheckbox">
-					<Form.Check type="checkbox" label="Check me out" />
 				</Form.Group>
 				<Button variant="primary" type="submit">
 					Submit
