e4042ae1497c8f51871d4f5b736e9b25a23ab272
AbigailGV <a01635153@itesm.mx>
2022-11-23 12:38:39 -0600

    basic template

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {Route, Routes} from 'react-router-dom'
+
+import {Login} from "./Login"
+
+const Home = () => <h2>Home</h2>
+const YourExchanges = () => <h2>Your Exchanges</h2>
+const YourResources = () => <h2>Your Resources</h2>
+
+function App()  {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/login' element={<Login />} ></Route>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/your-exchanges' element={<YourExchanges />} ></Route>
+        <Route path='/your-resources' element={<YourResources />} ></Route>
+      </Routes>
+    </div>
+  )
+}
+export default App
---
+++ src/Login.jsx
@@ -0,0 +1,9 @@
+//aqui va el login
+export const Login = ()=>  {
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      
+    </div>
+  )
+}
