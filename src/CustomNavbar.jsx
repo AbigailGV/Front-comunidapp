3a69b50ed032a7ccb2c7762247bcbfb992da2e79
AbigailGV <a01635153@itesm.mx>
2022-11-23 15:28:27 -0600

    tabs added in home

--- src/CustomNavbar.jsx
+++ src/CustomNavbar.jsx
@@ -3,21 +3,21 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import {BiUser} from "react-icons/bi";
+import { BiUser } from "react-icons/bi";
 
 export const CustomNavbar = () => {
-	return (
-		<Navbar bg="dark" variant="dark">
-			<Container className="">
-				<Navbar.Brand href="/">ComunidApp</Navbar.Brand>
-				<Nav className="">
-					<Nav.Link href="/yourexchanges">Your exchanges</Nav.Link>
-					<Nav.Link href="/yourresources">Your resources</Nav.Link>
-					<Nav.Link href="/profile">
-						<BiUser />
-					</Nav.Link>
-				</Nav>
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container className="">
+        <Navbar.Brand href="/">ComunidApp</Navbar.Brand>
+        <Nav className="">
+          <Nav.Link href="/your-exchanges">Your exchanges</Nav.Link>
+          <Nav.Link href="/your-resources">Your resources</Nav.Link>
+          <Nav.Link href="/profile">
+            <BiUser />
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,28 @@
 /* eslint-disable  */
 import React from "react";
 import { CustomNavbar } from "./CustomNavbar";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 export const Home = () => {
   return (
     <div>
       <CustomNavbar />
+      <Tabs
+        defaultActiveKey="alimentos"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="alimentos" title="Alimentos">
+          <h1>Contenido Alimentos</h1>
+        </Tab>
+        <Tab eventKey="medicinas" title="Medicinas">
+          <h1>Contenido Medicinas</h1>
+        </Tab>
+        <Tab eventKey="otros" title="Otros">
+          <h1>Contenido otros </h1>
+        </Tab>
+      </Tabs>
     </div>
   );
 };
