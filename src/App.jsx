c24fbf4a86e93f775a4790448aa63645415fed96
JorgeSalgado0999 <jorgesalgado_isc@outlook.com>
2022-11-23 14:57:25 -0600

    404

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
 /* eslint-disable  */
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
 
-import { Login } from "./Login";
-import { Home } from "./Home";
-import { YourExchanges } from "./YourExchanges";
-import { YourResources } from "./YourResources";
-import { Profile } from "./Profile";
+import {Login} from "./Login";
+import {Home} from "./Home";
+import {YourExchanges} from "./YourExchanges";
+import {YourResources} from "./YourResources";
+import {Profile} from "./Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/your-exchanges" element={<YourExchanges />}></Route>
-        <Route path="/your-resources" element={<YourResources />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/login" element={<Login />}></Route>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/your-exchanges" element={<YourExchanges />}></Route>
+				<Route path="/your-resources" element={<YourResources />}></Route>
+				<Route path="/profile" element={<Profile />}></Route>
+				<Route path="/*" element={<h1> 404 </h1>}></Route>
+			</Routes>
+		</div>
+	);
 }
 export default App;
