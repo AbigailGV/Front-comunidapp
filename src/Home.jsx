0a83c425a40e28c85badbbb99d38a6f9bffdddf9
AbigailGV <a01635153@itesm.mx>
2022-11-23 14:53:27 -0600

    Nav bar updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,24 +3,23 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import {BiUser} from "react-icons/bi";
+import { BiUser } from "react-icons/bi";
 
 export const Home = () => {
-	return (
-		<div>
-			<Navbar bg="dark" variant="dark">
-				<Container className="">
-					<Navbar.Brand href="/">ComunidApp</Navbar.Brand>
-					<Nav className="">
-						<Nav.Link href="#home"></Nav.Link>
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="/login">Pricing</Nav.Link>
-						<Nav.Link href="#home">
-							<BiUser />
-						</Nav.Link>
-					</Nav>
-				</Container>
-			</Navbar>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container className="">
+          <Navbar.Brand href="/">ComunidApp</Navbar.Brand>
+          <Nav className="">
+            <Nav.Link href="/yourexchanges">Your exchanges</Nav.Link>
+            <Nav.Link href="/yourresources">Your resources</Nav.Link>
+            <Nav.Link href="/profile">
+              <BiUser />
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
