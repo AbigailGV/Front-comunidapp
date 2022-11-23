93b9f5fec6c447229637e2b7fa4c7cf290c9b7c9
JorgeSalgado0999 <jorgesalgado_isc@outlook.com>
2022-11-23 13:03:18 -0600

    deleted trash files

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById("root")
+);
