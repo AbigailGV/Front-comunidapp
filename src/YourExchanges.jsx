613a0478cbc58a13a4057f6ca39574a6008cfdd4
AbigailGV <a01635153@itesm.mx>
2022-11-23 20:13:59 -0600

    table added on YourExchanges page

--- src/YourExchanges.jsx
+++ src/YourExchanges.jsx
@@ -1,13 +1,44 @@
 /* eslint-disable  */
 
-import {CustomNavbar} from "./CustomNavbar";
+import { CustomNavbar } from "./CustomNavbar";
+import Table from "react-bootstrap/Table";
 
 // Aquí van los exchanges del usuario :D
 export const YourExchanges = () => {
-	return (
-		<div className="App">
-			<CustomNavbar />
-			<h1>Your Exchanges</h1>
-		</div>
-	);
+  return (
+    <div className="App">
+      <CustomNavbar />
+      <h1>Your Exchanges</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Product</th>
+            <th>Amount</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Apples</td>
+            <td>3</td>
+            <td>@mdo09</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Water</td>
+            <td>10</td>
+            <td>@fatt65</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Juice</td>
+            <td>5</td>
+            <td>@twister3</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 };
--- src/YourResources.jsx
+++ src/YourResources.jsx
@@ -1,13 +1,37 @@
 /* eslint-disable  */
 
-import {CustomNavbar} from "./CustomNavbar";
+import { CustomNavbar } from "./CustomNavbar";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 // Aquí van los resources del usuario :D
 export const YourResources = () => {
-	return (
-		<div className="App">
-			<CustomNavbar />
-			<h1>Your resources</h1>
-		</div>
-	);
+  return (
+    <div className="App">
+      <CustomNavbar />
+      <div className="cards  mt-5">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src="https://estaticos-cdn.elperiodico.com/clip/e186c9b4-1d98-48f4-811f-a54a20b85dd3_alta-libre-aspect-ratio_default_0.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Water bottles</Card.Title>
+            <Card.Text>You have 20 bottles of water.</Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src="https://dam.cocinafacil.com.mx/wp-content/uploads/2018/03/cosas-que-no-sabias-sobre-el-platano.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Bananas</Card.Title>
+            <Card.Text>You have 15 bananas.</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 };
